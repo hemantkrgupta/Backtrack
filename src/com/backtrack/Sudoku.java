512e8116b4235c2821c8f197a3eb7406b2db6571
Hemant Kumar <hemant.gt.kumar@gmail.com>
2021-06-05 11:07:54 +0530

    Added Read Input

--- src/com/backtrack/Sudoku.java
+++ src/com/backtrack/Sudoku.java
@@ -1,5 +1,21 @@
 package com.backtrack;
 
-public class Sudoku {
+import java.util.Scanner;
 
+public class Sudoku {
+	public static void main(String... args){
+		int[][] sudoku = readInput();
+	}
+	
+	public static int[][] readInput(){
+		Scanner sc = new Scanner(System.in);
+		int[][] sudoku = new int[9][9];
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				sudoku[i][j] = sc.nextInt();
+			}
+		}
+		sc.close();
+		return sudoku;
+	}
 }
